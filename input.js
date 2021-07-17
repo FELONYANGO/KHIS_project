1a866b22ae8f46dc38462c22eb0b8fed06dd5aee
Kelly-001-maker <danielnyamaikeli@gmail.com>
2021-07-17 15:17:00 -0700

    data visualisation

--- input.js
+++ input.js
@@ -18,11 +18,14 @@
 
 //An event listener for submission
 document.getElementById('form_input').addEventListener('submit', submitForm);
-var f_name;
+// we declare the parameters we are getting from the input form as global
+var region;
+var region_name;
 function submitForm(e)
 {
     e.preventDefault();
-    f_name = document.getElementById('f_name').value;
+    region = document.getElementById('region').value;
+    region_name = document.getElementById('region_name').value;
     document.getElementById("my_button").disabled = true;
     retriveinfo();
 }
@@ -38,13 +41,181 @@ function retriveinfo()
 
 function gotdata(data)
 {
-    if(f_name=="hello")
+    var info = data.val();
+    var keys = Object.keys(info);
+    //Define some variables to take the freguencies for all levels
+    var level_1 = 0;
+    var level_2 = 0;
+    var level_3 = 0;
+    var level_4 = 0;
+    var level_5 = 0;
+
+    if(region=="county")
     {
-        console.log("hello");
+        for ( var i = 0; i < keys.length; i++)
+        {
+            //getting a key, the unique keys generated in firebase
+            var j = keys[i];
+            // getting the name value for county column
+            var county = info[j].county;
+            //getting the keph level for the facility
+            var keph_level = info.keph_level;
+            //if county name in database is equal to the county name entered do the following otherwise do nothing
+            if(county == region_name)
+            {
+               if(keph_level=="Level 1")
+               {
+                    // increment level 1 frequency
+                    level_1 = level_1 + 1; 
+                }
+                else if (keph_level=="Level 2")
+                {
+                    level_2 = level_2 + 1;
+                } 
+                else if (keph_level=="Level 3")
+                {
+                    level_3 = level_3 + 1;
+                } 
+                else if (keph_level=="Level 4")
+                {
+                    level_4 = level_4 + 1;
+                } 
+                else if (keph_level=="Level 5")
+                {
+                     level_5 = level_5 + 1;
+                 } 
+                else 
+                {
+                  //do nothing, it means the levels were not entered as expected  
+                }
+            }
+        }
+
+    }
+    else if(region=="sub_county")
+    {
+        for ( var i = 0; i < keys.length; i++)
+        {
+            //getting a key, the unique keys generated in firebase
+            var j = keys[i];
+            // getting the name value for county column
+            var sub_county = info[j].sub_county;
+            //getting the keph level for the facility
+            var keph_level = info.keph_level;
+            //if county name in database is equal to the county name entered do the following otherwise do nothing
+            if(sub_county == region_name)
+            {
+               if(keph_level=="Level 1")
+               {
+                    // increment level 1 frequency
+                    level_1 = level_1 + 1; 
+                }
+                else if (keph_level=="Level 2")
+                {
+                    level_2 = level_2 + 1;
+                } 
+                else if (keph_level=="Level 3")
+                {
+                    level_3 = level_3 + 1;
+                } 
+                else if (keph_level=="Level 4")
+                {
+                    level_4 = level_4 + 1;
+                } 
+                else if (keph_level=="Level 5")
+                {
+                     level_5 = level_5 + 1;
+                 } 
+                else 
+                {
+                  //do nothing, it means the levels were not entered as expected  
+                }
+            }
+        }
+    } 
+    else if(region=="ward")
+    {   
+        for ( var i = 0; i < keys.length; i++)
+        {
+            //getting a key, the unique keys generated in firebase
+            var j = keys[i];
+            // getting the name value for county column
+            var ward = info[j].ward;
+            //getting the keph level for the facility
+            var ward = info.ward;
+            //if county name in database is equal to the county name entered do the following otherwise do nothing
+            if(sub_county == region_name)
+            {
+               if(keph_level=="Level 1")
+               {
+                    // increment level 1 frequency
+                    level_1 = level_1 + 1; 
+                }
+                else if (keph_level=="Level 2")
+                {
+                    level_2 = level_2 + 1;
+                } 
+                else if (keph_level=="Level 3")
+                {
+                    level_3 = level_3 + 1;
+                } 
+                else if (keph_level=="Level 4")
+                {
+                    level_4 = level_4 + 1;
+                } 
+                else if (keph_level=="Level 5")
+                {
+                     level_5 = level_5 + 1;
+                 } 
+                else 
+                {
+                  //do nothing, it means the levels were not entered as expected  
+                }
+            }
+        }
     }
     else
     {
-        console.log("Nooooo");
+     console.log("region does not match any of us")
     }
- 
-}
+  // i just update the levels falsely for test
+  level_1 = 6;
+  level_2 = 3;
+  level_3 = 5;
+  level_4 = 7;
+  level_5 = 4;
+
+    // Now at this point we can execute the code to display a graph
+
+        title_name = region_name+"  "+region
+        y_axis_text = "Frequency of KPEH Levels in "+title_name
+        var chartData = {
+          type: 'bar',
+          "title":{  
+             "text": title_name  
+             }, 
+             "scale-y":{  
+                label: { /* Scale Title */
+                  text: y_axis_text,
+                  },
+                  values: "0:8:1"
+                },
+             "scale-x":{  
+                label: { /* Scale Title */
+                  text: "KEPH Levels",
+                  },
+                  labels: ["Level 1", "Level 2 ", "Level 3","Level 4", "Level 5"]
+                },
+          series: [
+            { 
+                values: [level_1, level_2, level_3, level_4, level_5],
+                'background-color': "#6666FF"
+            },
+          ]
+           };
+        zingchart.render({
+          id: 'myChart',
+          data: chartData,
+          width: "80%"
+        });
+  }  
